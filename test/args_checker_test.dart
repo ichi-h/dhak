5d2866aaa25a76a9ce7c410bb721a018c6a0fe5e
ippee <ippee.music@gmail.com>
2021-07-10 23:36:59 +0900

    Add test code of args_checker.dart

---
+++ test/args_checker_test.dart
@@ -0,0 +1,83 @@
+import 'package:test/test.dart';
+import 'package:dhak/args_checker.dart' show ArgsChecker;
+import 'package:dhak/dhak_exception.dart' show DhakSyntaxException;
+
+void main() {
+  group('Normal behavior', () {
+    var args;
+    ArgsChecker checker;
+
+    test('Args: ["-h"]', () {
+      args = ['-h'];
+      checker = new ArgsChecker(args);
+      final result = checker.checkedArgs();
+      expect(result, equals(args));
+    });
+
+    test('Args: ["GitHub"]', () {
+      args = ['GitHub'];
+      checker = new ArgsChecker(args);
+      final result = checker.checkedArgs();
+      expect(result, equals(args));
+    });
+
+    test('Args: ["GitHub", "default"]', () {
+      args = ['GitHub', 'default'];
+      checker = new ArgsChecker(args);
+      final result = checker.checkedArgs();
+      expect(result, equals(args));
+    });
+
+    test('Args: ["GitHub", "-s"]', () {
+      args = ['GitHub', '-s'];
+      checker = new ArgsChecker(args);
+      final result = checker.checkedArgs();
+      expect(result, equals(args));
+    });
+
+    test('Args: ["GitHub", "default", "-s"]', () {
+      args = ['GitHub', 'default', '-s'];
+      checker = new ArgsChecker(args);
+      final result = checker.checkedArgs();
+      expect(result, equals(args));
+    });
+  });
+
+  group('Abnormal behavior', () {
+    var args;
+    ArgsChecker checker;
+
+    test('Args: ["-s", "GitHub"]', () {
+      args = ['-s', 'GitHub', 'default'];
+      checker = new ArgsChecker(args);
+      try {
+        checker.check();
+      } on DhakSyntaxException {
+        return;
+      }
+      fail('The command-line with syntax error passes the test.');
+    });
+
+    test('Args: ["-s", "GitHub", "default"]', () {
+      args = ['-s', 'GitHub', 'default'];
+      checker = new ArgsChecker(args);
+      try {
+        checker.check();
+      } on DhakSyntaxException {
+        return;
+      }
+      fail('The command-line with syntax error passes the test.');
+    });
+
+    test('Args: ["GitHub", "-s", "default"]', () {
+      args = ['GitHub', '-s', 'default'];
+      checker = new ArgsChecker(args);
+      try {
+        checker.check();
+      } on DhakSyntaxException {
+        return;
+      }
+      fail('The command-line with syntax error passes the test.');
+    });
+  });
+}
