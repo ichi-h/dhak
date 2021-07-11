fbd1786881481903582193900b3d308ef90d22ce
ippee <ippee.music@gmail.com>
2021-07-11 15:39:13 +0900

    Add Cmd and its subclasses.

---
+++ lib/cmd/generate.dart
@@ -0,0 +1,14 @@
+import 'package:dhak/cmd/cmd.dart';
+
+class Generate extends Cmd {
+  final String title;
+  final String preset;
+  final String option;
+
+  Generate(this.title, this.preset, this.option);
+
+  @override
+  void run() {
+    // TODO: implement run
+  }
+}
---
+++ lib/cmd/help.dart
@@ -0,0 +1,12 @@
+import 'package:dhak/cmd/cmd.dart';
+
+class Help extends Cmd {
+  final String option;
+
+  Help(this.option);
+
+  @override
+  void run() {
+    // TODO: implement run
+  }
+}
