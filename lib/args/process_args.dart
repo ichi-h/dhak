f4febc311e7b31cbb27e1933d7729e18188b4caf
ippee <ippee.music@gmail.com>
2021-07-12 23:06:21 +0900

    Merge SeparateArgs and SyntaxChecker.
    The fact that the checker clearly understands the syntax means that it also understands the separation of arguments, so essentially they both do the same thing.

--- lib/args/process_args.dart
+++ lib/args/process_args.dart
@@ -1,25 +1,13 @@
 class ProcessArgs {
   final List<String> args;
+
   ProcessArgs(this.args);
 
   bool isOption(int index) {
     return this.args[index].startsWith('-');
   }
 
-  bool isOptionHelp(int index) {
-    return this.args[index] == '-h' || this.args[index] == '--help';
-  }
-
   int optionIndex() {
-    return this.args.indexWhere(
-            (arg) => arg.startsWith('-')
-    );
-  }
-
-  bool isHelp() {
-    if (this.args.length == 1 && this.isOptionHelp(0)) {
-      return true;
-    }
-    return false;
+    return this.args.indexWhere((arg) => arg.startsWith('-'));
   }
 }
--- lib/args/syntax_checker.dart
+++ lib/args/syntax_checker.dart
@@ -15,47 +15,70 @@ class SyntaxChecker {
       return this.check();
     } on DhakSyntaxException catch (e) {
       print(e.message);
-      return ['--help'];
+      return ['', '', '--help'];
     }
   }
 
   List<String> check() {
-    if (3 < this.args.length) {
-      throw new DhakSyntaxException('Syntax error: The length of the command-line arguments must be less than or equal to 3.\n');
-    }
+    final result;
 
     switch (this.args.length) {
       case 1:
-        if (this._procArgs.isOption(0) && !this._procArgs.isOptionHelp(0)) {
-          this._hyphenException();
-        }
-
+        result = this._onOne();
         break;
 
       case 2:
-        if (this._procArgs.isOption(0)) {
-          this._hyphenException();
-        }
-
+        result = this._onTwo();
         break;
 
       case 3:
-        if (this._procArgs.isOption(0) || this._procArgs.isOption(1)) {
-          this._hyphenException();
-        }
-
-        if (this._procArgs.optionIndex() == -1) {
-          var errorArg = this.args[2];
-          throw new DhakSyntaxException('Syntax error: The argument "$errorArg (2)" does not match the syntax of dhak.\n');
-        }
-
+        result = this._onThree();
         break;
+
+      default:
+        throw new DhakSyntaxException(
+            'Syntax error: The length of the command-line arguments must be less than or equal to 3.\n');
+    }
+
+    return result;
+  }
+
+  List<String> _onOne() {
+    if (this._procArgs.isOption(0)) {
+      return ['', '', this.args[0]];
+    }
+
+    return [this.args[0], 'default', ''];
+  }
+
+  List<String> _onTwo() {
+    if (this._procArgs.isOption(0)) {
+      this._hyphenException();
+    }
+
+    if (this._procArgs.isOption(1)) {
+      return [this.args[0], 'default', this.args[1]];
+    }
+
+    return [this.args[0], this.args[1], ''];
+  }
+
+  List<String> _onThree() {
+    if (this._procArgs.isOption(0) || this._procArgs.isOption(1)) {
+      this._hyphenException();
+    }
+
+    if (this._procArgs.optionIndex() == -1) {
+      var errorArg = this.args[2];
+      throw new DhakSyntaxException(
+          'Syntax error: The argument "$errorArg (2)" does not match the syntax of dhak.\n');
     }
 
     return this.args;
   }
 
   void _hyphenException() {
-    throw new DhakSyntaxException('Syntax error: Hyphens are not allowed in the first character of the <title> and <preset>.\n');
+    throw new DhakSyntaxException(
+        'Syntax error: Hyphens are not allowed in the first character of the <title> and <preset>.\n');
   }
 }
