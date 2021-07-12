4693380957d9a91f050818341c66f3f7a227c18d
ippee <ippee.music@gmail.com>
2021-07-12 23:30:06 +0900

    Add DhakRuntimeException class.

--- lib/dhak_exception.dart
+++ lib/dhak_exception.dart
@@ -2,3 +2,8 @@ class DhakSyntaxException implements Exception {
   final String message;
   DhakSyntaxException(this.message);
 }
+
+class DhakRuntimeException implements Exception {
+  final String message;
+  DhakRuntimeException(this.message);
+}
