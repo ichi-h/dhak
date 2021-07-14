3b4f93a86013a4be18d106fa276647a3e88047c1
ippee <ippee.music@gmail.com>
2021-07-14 22:33:05 +0900

    Implement hasSymbols and isSecure methods.

--- lib/cmd/generate/password_operator.dart
+++ lib/cmd/generate/password_operator.dart
@@ -1,5 +1,7 @@
 import 'dart:math';
 
+import 'package:dhak/util/global_const.dart';
+
 class PasswordOperator {
   final String password;
 
@@ -22,5 +24,56 @@ class PasswordOperator {
     }
 
     return result;
+  }
+
+  bool hasSymbols() {
+    var result = this.password;
+
+    for (var char in GlobalConst.LOWER_CASE) {
+      result = result.replaceAll(char, '');
+    }
+
+    for (var char in GlobalConst.UPPER_CASE) {
+      result = result.replaceAll(char, '');
+    }
+
+    for (var num in GlobalConst.NUMBERS) {
+      result = result.replaceAll(num.toString(), '');
+    }
+
+    if (0 < result.length) return true;
+
+    return false;
+  }
+
+  bool isSecure() {
+    var result = {'lowerCase': false, 'upperCase': false, 'numbers': false};
+
+    for (var char in GlobalConst.LOWER_CASE) {
+      if (this.password.contains(char)) {
+        result['lowerCase'] = true;
+        break;
+      }
+    }
+
+    for (var char in GlobalConst.UPPER_CASE) {
+      if (this.password.contains(char)) {
+        result['upperCase'] = true;
+        break;
+      }
+    }
+
+    for (var num in GlobalConst.NUMBERS) {
+      if (this.password.contains(num.toString())) {
+        result['numbers'] = true;
+        break;
+      }
+    }
+
+    if (result['lowerCase']! && result['upperCase']! && result['numbers']!) {
+      return true;
+    }
+
+    return false;
   }
 }
