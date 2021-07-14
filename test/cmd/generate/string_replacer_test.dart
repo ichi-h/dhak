a797b84f50997db9c5388ecba5d48c8938f4e485
ippee <ippee.music@gmail.com>
2021-07-14 18:57:11 +0900

    Update string_replacer_test.dart

--- test/cmd/generate/string_replacer_test.dart
+++ test/cmd/generate/string_replacer_test.dart
@@ -3,15 +3,23 @@ import 'package:test/test.dart';
 
 void main() {
   group('Normal behavior', () {
-    test('', () {
+    test('Replace char of password by symbols', () {
       var password = "mq55vn43JnIQSQnh3YJL";
       var symbols = ['/', '#', '%', '@', '+', '-', '_'];
 
-      var rr = RandomReplacer(password);
-      var processed = rr.replaceBy(symbols);
+      var result = ['', ''];
+      for (var i = 0; i < 2; i++) {
+        var rr = RandomReplacer(password);
+        result[i] = rr.replaceBy(symbols);
+      }
 
-      if (password == processed) {
-        fail('String was not replaced.');
+      if (result[0] != result[1]) {
+        fail(
+            'Different passwords have been generated under the same conditions.');
+      }
+
+      if (result[0] == password) {
+        fail('Password was not replaced.');
       }
     });
   });
