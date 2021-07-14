57e40ce2c594e60702d07e9c52e1a82efaeab3eb
ippee <ippee.music@gmail.com>
2021-07-14 22:31:57 +0900

    Add new tests.

--- test/cmd/generate/password_operator_test.dart
+++ test/cmd/generate/password_operator_test.dart
@@ -3,14 +3,16 @@ import 'package:test/test.dart';
 
 void main() {
   group('Normal behavior', () {
+    PasswordOperator operator;
+
     test('Replace char of password with symbols', () {
-      var password = "mq55vn43JnIQSQnh3YJL";
+      var password = 'mq55vn43JnIQSQnh3YJL';
       var symbols = ['/', '#', '%', '@', '+', '-', '_'];
 
       var result = ['', ''];
       for (var i = 0; i < 2; i++) {
-        var rr = PasswordOperator(password);
-        result[i] = rr.replaceAtRandomWith(symbols);
+        operator = PasswordOperator(password);
+        result[i] = operator.replaceAtRandomWith(symbols);
       }
 
       if (result[0] != result[1]) {
@@ -21,6 +23,30 @@ void main() {
       if (result[0] == password) {
         fail('Password was not replaced.');
       }
+    });
+
+    test('Has symbols', () {
+      var password = r"PQqQaZ5I'mmw6.\h`\a;";
+      operator = PasswordOperator(password);
+      expect(operator.hasSymbols(), equals(true));
+    });
+
+    test('Not have symbols', () {
+      var password = 'j8QfVGvzstJJShiAfx8o';
+      operator = PasswordOperator(password);
+      expect(operator.hasSymbols(), equals(false));
+    });
+
+    test('Has lower case, upper case and number', () {
+      var password = 'L2ydGSzxay7pLG6BJK7S';
+      operator = PasswordOperator(password);
+      expect(operator.isSecure(), equals(true));
+    });
+
+    test('Not have lower case, upper case and number', () {
+      var password = 'l2ydgszxay7plg6bjk7s'; // not have upper case letter
+      operator = PasswordOperator(password);
+      expect(operator.isSecure(), equals(false));
     });
   });
 }
