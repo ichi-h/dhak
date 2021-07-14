4c4c3404ecd65b49f20e0546058f4081ce415d34
ippee <ippee.music@gmail.com>
2021-07-14 17:25:07 +0900

    Remove line breaks.

--- lib/cmd/help_cmd.dart
+++ lib/cmd/help_cmd.dart
@@ -6,18 +6,15 @@ class HelpCmd extends Cmd {
     print(
         '''Usage: dhak [-v, --version] [-h, --help] <title> (<preset>) [option]
 
-dhak combines your original passphrase with the name of the service that
-requires a password, hashes it, and uses the value as the new password.
+dhak combines your original passphrase with the name of the service that requires a password, hashes it, and uses the value as the new password.
 The generated password will be copied to the clipboard.
 
-The syntax of dhak basically consists of three parts: title, preset, and
-option.
+The syntax of dhak basically consists of three parts: title, preset, and option.
 
 title:
   The name of the service.
 preset:
-  The pre-prepared setting for password generation (you can add your own
-  presets in ~/.dhakrc).
+  The pre-prepared setting for password generation (you can add your own presets in ~/.dhakrc).
   If it is omitted, the value will be "default".
 option:
   Specify additional functions as needed.
