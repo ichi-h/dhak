92f3a57956f4e6dd82557d8e5613aa247d41c53b
ippee <ippee.music@gmail.com>
2021-07-14 19:23:47 +0900

    Implement RandomReplacer class.

--- lib/cmd/generate/random_replacer.dart
+++ lib/cmd/generate/random_replacer.dart
@@ -1,1 +1,29 @@
-class RandomReplacer {}
+import 'dart:math';
+
+class RandomReplacer {
+  final String password;
+
+  RandomReplacer(this.password);
+
+  String replaceBy(List<String> symbols) {
+    final rnd = Random(this.password.hashCode);
+    final passLen = this.password.length;
+
+    // Calculate trials of replacements.
+    // It is adjusted to be from 1 to a quarter of password length.
+    final trials = 1 + rnd.nextInt(passLen) % (passLen / 4).floor();
+
+    var result = this.password;
+
+    for (var i = 0; i < trials; i++) {
+      var targetIndex = rnd.nextInt(passLen);
+      var symbol = symbols[rnd.nextInt(symbols.length)];
+      result = result.replaceRange(targetIndex, targetIndex + 1, symbol);
+    }
+
+    print(this.password);
+    print(result);
+
+    return result;
+  }
+}
