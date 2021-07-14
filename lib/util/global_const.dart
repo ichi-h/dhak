4984620d443936d5e2032c39308e851c71b2ecac
ippee <ippee.music@gmail.com>
2021-07-14 22:09:24 +0900

    Add char const.

--- lib/util/global_const.dart
+++ lib/util/global_const.dart
@@ -1,3 +1,63 @@
 class GlobalConst {
   static const VERSION = '0.1.0';
+
+  static const LOWER_CASE = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+
+  static const UPPER_CASE = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
+
+  static const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 }
