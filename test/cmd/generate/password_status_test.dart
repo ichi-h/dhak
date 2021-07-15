4ebfeab2ed316402c78f05db128ffa2acf6112d1
ippee <ippee.music@gmail.com>
2021-07-16 00:22:03 +0900

    Fix expected value.

--- test/cmd/generate/password_status_test.dart
+++ test/cmd/generate/password_status_test.dart
@@ -8,25 +8,25 @@ void main() {
     test('Has symbols', () {
       var password = r"PQqQaZ5I'mmw6.\h`\a;";
       info = PasswordStatus(password);
-      expect(info.hasSymbols(), equals(true));
+      expect(info.hasSymbols(), isTrue);
     });
 
     test('Not have symbols', () {
       var password = 'j8QfVGvzstJJShiAfx8o';
       info = PasswordStatus(password);
-      expect(info.hasSymbols(), equals(false));
+      expect(info.hasSymbols(), isFalse);
     });
 
     test('Has lower case, upper case and number', () {
       var password = 'L2ydGSzxay7pLG6BJK7S';
       info = PasswordStatus(password);
-      expect(info.isSecure(), equals(true));
+      expect(info.isSecure(), isTrue);
     });
 
     test('Not have lower case, upper case and number', () {
       var password = 'l2ydgszxay7plg6bjk7s'; // not have upper case letter
       info = PasswordStatus(password);
-      expect(info.isSecure(), equals(false));
+      expect(info.isSecure(), isFalse);
     });
   });
 }
