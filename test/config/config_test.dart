25376abc7dbcf20d9e55a83ad4ec335431c149ce
ippee <ippee.music@gmail.com>
2021-07-15 20:56:02 +0900

    Add test of Config.

---
+++ test/config/config_test.dart
@@ -0,0 +1,30 @@
+import 'package:dhak/config/config.dart';
+import 'package:test/test.dart';
+
+void main() {
+  Config config;
+
+  group('Normal behavior', () {
+    test('Get default preset', () {
+      config = Config('./config.yaml');
+      var preset = config.getPreset('default');
+
+      var result = {
+        'presetName': preset.name,
+        'passLen': preset.passLength,
+        'symbols': preset.symbols,
+        'salt': preset.salt
+      };
+      var expected = {
+        'presetName': 'default',
+        'passLen': 20,
+        'symbols': ["/", "#", "%", "&", "@", "+", "-", "_"],
+        'salt': ''
+      };
+
+      expect(result, equals(expected));
+    });
+  });
+
+  // group('Abnormal behavior', () {});
+}
