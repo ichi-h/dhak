a5422b41b4f33fce0e3b4025bce0664c00594993
ippee <ippee.music@gmail.com>
2021-07-17 19:15:22 +0900

    Add Options class.

---
+++ lib/args/options.dart
@@ -0,0 +1,15 @@
+class Options {
+  final List<String> options;
+
+  Options(this.options);
+
+  bool hasForce() {}
+
+  bool hasDisplay() {}
+
+  int passLength() {}
+
+  String algorithm() {}
+
+  String cost() {}
+}
