e93eab62b36c0ae521c6bfd98800d58bd9471404
ippee <ippee.music@gmail.com>
2021-07-18 17:04:35 +0900

    Separate detailed processes.

--- lib/cmd/generate/password_gen.dart
+++ lib/cmd/generate/password_gen.dart
@@ -16,8 +16,8 @@ class PasswordGen {
 
   String getPassword() {
     var force = this._options.exist(OptionTarget.force);
-
     var password = this._genPassword(force);
+
     final rnd = Random(password.hashCode);
     PasswordStatus status = PasswordStatus(password);
     while (!status.isSecure(force)) {
@@ -30,45 +30,71 @@ class PasswordGen {
   }
 
   String _genPassword(bool force, [int rndInt = 0]) {
+    final salt = this._getSalt();
+
+    // Add hash until password exceeds passLength.
+    var hash = '';
+    var encrypted = '$rndInt${this._target}';
+    var len = this._getPassLength(force);
+    while (hash.length < len) {
+      encrypted = DBCrypt().hashpw(encrypted, salt);
+
+      // Extract only hash value.
+      for (var i = encrypted.length - 1; 29 <= i; i--) {
+        hash += encrypted[i];
+      }
+    }
+
+    var password = hash.substring(0, len);
+
+    // Remove symbols which exist from the beginning.
+    final operator = PasswordOperator(password);
+    password = operator.replaceSymbols();
+
+    var symbols = this._getSymbols();
+    if (symbols.length != 0) {
+      password = operator.replaceAtRandomWith(symbols);
+    }
+
+    return password;
+  }
+
+  int _getPassLength(bool force) {
     var len = this._preset.passLength().value(force);
     if (this._options.exist(OptionTarget.len)) {
       len = this._options.passLength().value(force);
     }
+    return len;
+  }
 
+  List<String> _getSymbols() {
+    var sym = this._preset.symbols().value();
+    if (this._options.exist(OptionTarget.sym)) {
+      sym = this._options.symbols().value();
+    }
+    return sym;
+  }
+
+  String _getSalt() {
+    var algo = this._getAlgo();
+    var cost = this._getCost();
+    var hashCode = this._target.hashCode;
+    return '\$$algo\$$cost\$${GenSalt.fromHashCode(hashCode)}';
+  }
+
+  String _getAlgo() {
     var algo = this._preset.algorithm().value();
-    var cost = this._preset.cost().value();
     if (this._options.exist(OptionTarget.algo)) {
       algo = this._options.algorithm().value();
     }
+    return algo;
+  }
+
+  String _getCost() {
+    var cost = this._preset.cost().value();
     if (this._options.exist(OptionTarget.cost)) {
       cost = this._options.cost().value();
     }
-
-    var hashCode = this._target.hashCode;
-    final salt = '\$$algo\$$cost\$${GenSalt.fromHashCode(hashCode)}';
-
-    var reversed = '';
-    var encrypted = '$rndInt${this._target}';
-    while (reversed.length < len) {
-      encrypted = DBCrypt().hashpw(encrypted, salt);
-      for (var i = encrypted.length - 1; 29 <= i; i--) {
-        reversed += encrypted[i];
-      }
-    }
-
-    var symbols = this._preset.symbols().value();
-    if (this._options.exist(OptionTarget.sym)) {
-      symbols = this._options.symbols().value();
-    }
-
-    var password = reversed.substring(0, len);
-    final operator = PasswordOperator(password);
-    if (symbols.length != 0) {
-      password = operator.replaceAtRandomWith(symbols);
-    } else {
-      password = operator.replaceSymbols();
-    }
-
-    return password;
+    return cost;
   }
 }
