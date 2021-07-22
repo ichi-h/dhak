556b7f15d4cd1a741bedcec922fe2a2b05f04fe8
ippee <ippee.music@gmail.com>
2021-07-22 11:32:34 +0900

    Change Config class to one thatassumes that .dhakrc exists.

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,25 +1,16 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:dhak/config/default_preset.dart';
 import 'package:dhak/config/preset.dart';
 import 'package:dhak/util/dhak_exception.dart';
 
 class Config {
-  late final file;
   late Map<String, dynamic> _doc;
 
   Config(String path) {
-    this.file = File(path);
-
-    if (!this.file.existsSync()) {
-      print('"$path" was not found.');
-      print('Creating "$path" now...');
-      var json = Config._stringConfig();
-      this.file.writeAsStringSync(json);
-      print('Finished!\n');
-    }
-
-    var contents = this.file.readAsStringSync();
+    var file = File(path);
+    var contents = file.readAsStringSync();
     this._doc = json.decode(contents);
   }
 
@@ -31,29 +22,16 @@ class Config {
     }
 
     String? passLength = preset['password_length'];
-    passLength ??= '20';
+    passLength ??= DefaultPreset.passLength;
 
     String? symbols = preset['symbols'];
-    symbols ??= r'!"#$%&‘()*+,-./:;<=>?@[\]^_`{|}~';
+    symbols ??= DefaultPreset.symbols;
 
     String? algo = preset['algorithm'];
     String? cost = preset['cost'];
-    algo ??= '2b';
-    cost ??= '10';
+    algo ??= DefaultPreset.algo;
+    cost ??= DefaultPreset.cost;
 
     return Preset(presetName, passLength, symbols, algo, cost);
-  }
-
-  static String _stringConfig() {
-    return r'''{
-  "presets": {
-    "default": {
-      "password_length": "20",
-      "symbols": "!\"#$%&‘()*+,-./:;<=>?@[\\]^_`{|}~",
-      "algorithm": "2b",
-      "cost": "10"
-    }
-  }
-}''';
   }
 }
---
+++ lib/config/default_preset.dart
@@ -0,0 +1,6 @@
+class DefaultPreset {
+  static const String passLength = '20';
+  static const String symbols = r'!"#$%&‘()*+,-./:;<=>?@[\]^_`{|}~';
+  static const String algo = '2b';
+  static const String cost = '10';
+}
